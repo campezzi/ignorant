4e928c08a46e68e4e1bc3e1decb4b0bf7143a04e
Thiago Campezzi <campezzi@gmail.com>
2016-08-23 21:41:44 +1000

    remove unnecessary guard clause

--- lib/ignorant_map.ex
+++ lib/ignorant_map.ex
@@ -4,16 +4,16 @@ defimpl Ignorant, for: Map do
     |> Enum.reduce(map, &do_ignore/2)
   end
 
-  def do_ignore({key, ignored_fields}, acc) when is_list(ignored_fields) do
+  def do_ignore({key, ignored_fields}, source_map) do
     ignored =
-      case acc[key] do
+      case source_map[key] do
         list when is_list(list) -> Enum.map(list, fn item -> ignore(item, ignored_fields) end)
         map when is_map(map) -> ignore(map, ignored_fields)
       end
 
-    %{acc | key => ignored}
+    %{source_map | key => ignored}
   end
-  def do_ignore(ignored_field, acc) do
-    %{acc | ignored_field => :ignored}
+  def do_ignore(ignored_field, source_map) do
+    %{source_map | ignored_field => :ignored}
   end
 end
