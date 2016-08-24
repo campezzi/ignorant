57199dc11dc101895759cf73af9fb8eb9f22e78d
Thiago Campezzi <campezzi@gmail.com>
2016-08-24 19:51:52 +1000

    ensure ignore works on maps that use strings as keys

--- lib/ignorant_map.ex
+++ lib/ignorant_map.ex
@@ -5,15 +5,24 @@ defimpl Ignorant, for: Map do
   end
 
   def do_ignore({key, ignored_fields}, source_map) do
+    {k, v} = fetch(source_map, key)
+
     ignored =
-      case source_map[key] do
+      case v do
         list when is_list(list) -> Enum.map(list, fn item -> ignore(item, ignored_fields) end)
         map when is_map(map) -> ignore(map, ignored_fields)
       end
 
-    %{source_map | key => ignored}
+    %{source_map | k => ignored}
   end
   def do_ignore(ignored_field, source_map) do
     %{source_map | ignored_field => :ignored}
+  end
+
+  defp fetch(map, key) do
+    case map[key] do
+      nil -> {to_string(key), map[to_string(key)]}
+      value -> {key, value}
+    end
   end
 end
--- test/ignorant_map_test.exs
+++ test/ignorant_map_test.exs
@@ -4,6 +4,7 @@ defmodule IgnorantMapTest do
   describe "ignore/2" do
     test "returns the map with :ignored tags on specified fields" do
       map = %{name: "Jim", age: 28, company: "Dunder Mifflin"}
+
       expected = %{map | age: :ignored, company: :ignored}
 
       assert Ignorant.ignore(map, [:age, :company]) == expected
@@ -12,19 +13,41 @@ defmodule IgnorantMapTest do
     test "recursively adds :ignored tags on nested maps" do
       nested_map = %{email: "a@a.com", phone: "123456"}
       map = %{name: "Jim", age: 28, contacts: nested_map}
-      expected = %{map | age: :ignored, contacts: %{nested_map | phone: :ignored}}
+
+      expected_nested_map = %{nested_map | phone: :ignored}
+      expected = %{map | age: :ignored, contacts: expected_nested_map}
 
       assert Ignorant.ignore(map, [:age, contacts: [:phone]]) == expected
     end
 
     test "recursively adds :ignored tags on nested lists" do
-      item = fn x -> %{title: x, details: x} end
-      nested_list = [item.("a"), item.("b")]
+      nested_list = [%{title: "a", details: "a"}, %{title: "b", details: "b"}]
       map = %{name: "Jim", age: 28, posts: nested_list}
+
       expected_nested_list = Enum.map(nested_list, fn item -> %{item | details: :ignored} end)
       expected = %{map | age: :ignored, posts: expected_nested_list}
 
       assert Ignorant.ignore(map, [:age, posts: [:details]]) == expected
+    end
+
+    test "works if the map has strings as keys" do
+      map = %{"name" => "Jim", "age" => 28, "company" => "Dunder Mifflin"}
+
+      expected = %{map | "age" => :ignored, "company" => :ignored}
+
+      assert Ignorant.ignore(map, ["age", "company"]) == expected
+    end
+
+    test "recursion works if the map has string as keys" do
+      nested_map = %{"email" => "a@a.com", "phone" => "123456"}
+      nested_list = [%{"title" => "a", "details" => "a"}, %{"title" => "b", "details" => "b"}]
+      map = %{"name" => "Jim", "age" => 28, "contacts" => nested_map, "posts" => nested_list}
+
+      expected_nested_map = %{nested_map | "phone" => :ignored}
+      expected_nested_list = Enum.map(nested_list, fn item -> %{item | "details" => :ignored} end)
+      expected = %{map | "age" => :ignored, "contacts" => expected_nested_map, "posts" => expected_nested_list}
+
+      assert Ignorant.ignore(map, ["age", "contacts": ["phone"], "posts": ["details"]]) == expected
     end
   end
 end
